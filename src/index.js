d0051475237687fd90954b39a977132a743484a3
RadekM86 <byrm.design@gmail.com>
2018-01-16 19:59:36 +0100

    added scroll down and up buttons for smooth scrolling animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-console.log("page loaded")
-    
-})
+    $(document).ready(function() {
+        $(".down").click(function() {
+             $('html, body').animate({
+                 scrollTop: $("footer").offset().top
+             }, 1500);
+         });
+        });
+        
+        $(document).ready(function() {
+        $(".up").click(function() {
+             $('html, body').animate({
+                 scrollTop: $(".wrapper").offset().top
+             }, 1000);
+         });
+        });
+        }
+)
