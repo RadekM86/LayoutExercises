8c3356e7ba341fa0536ff2e16678d9d97ac0e9c6
RadekM86 <byrm.design@gmail.com>
2018-01-17 19:14:30 +0100

    Added animated motto and jumping scroll down button

--- out.js
+++ out.js
@@ -544,9 +544,23 @@ module.exports = __webpack_require__(3);
 /***/ (function(module, exports) {
 
 document.addEventListener("DOMContentLoaded", function() {
-console.log("page loaded")
-    
-})
+    $(document).ready(function() {
+        $(".down").click(function() {
+             $('html, body').animate({
+                 scrollTop: $("footer").offset().top
+             }, 1500);
+         });
+        });
+        
+        $(document).ready(function() {
+        $(".up").click(function() {
+             $('html, body').animate({
+                 scrollTop: $(".wrapper").offset().top
+             }, 1000);
+         });
+        });
+        }
+)
 
 
 /***/ }),
--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
+import { setInterval } from "timers";
+
+
 document.addEventListener("DOMContentLoaded", function() {
     $(document).ready(function() {
         $(".down").click(function() {
              $('html, body').animate({
                  scrollTop: $("footer").offset().top
              }, 1500);
+             $(".down").css("display", "none")
+             $(".up").css("display", "block")
+            });
          });
-        });
-        
-        $(document).ready(function() {
         $(".up").click(function() {
              $('html, body').animate({
                  scrollTop: $(".wrapper").offset().top
              }, 1000);
-         });
-        });
-        }
-)
+             $(".down").css("display", "block")
+             $(".up").css("display", "none")
+            });
+        var motto = $('.motto')
+        var counter = 0;
+         function text(){
+             counter++;
+                motto.fadeOut(2500, function(){
+                    motto.fadeIn(2500).text((counter%2===0)?"Sit comfortably":"In our Chairs")
+                })
+            };
+        setInterval(text, 5000)
+        
+        
+                 
+
+
+                                            
+                        
+                        
+            
+
+});
+         
