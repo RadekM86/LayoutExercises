ad382c43b0d91901d9fda7b7d10f865bd11bf1ab
RadekM86 <byrm.design@gmail.com>
2018-01-22 19:41:00 +0100

    Added animated drawing line svg

--- src/index.js
+++ src/index.js
@@ -40,10 +40,20 @@ document.addEventListener("DOMContentLoaded", function() {
                 e.preventDefault()
            })
 
-                                        
-                        
-                        
-            
 
+  //variable for the 'stroke-dashoffset' unit
+  var $dashOffset = $(".path").css("stroke-dashoffset");
+  //on a scroll event - execute function
+  $(window).scroll(function() {
+    //calculate how far down the page the user is 
+    var $percentageComplete = (($(window).scrollTop() / ($("html").height())) * 100);
+    //convert dashoffset pixel value to interger
+    var $newUnit = parseInt($dashOffset, 10);
+    //get the value to be subtracted from the 'stroke-dashoffset'
+    var $offsetUnit = $percentageComplete * ($newUnit / 100);
+    //set the new value of the dashoffset to create the drawing effect
+    $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);
+  });
 });
+
          
